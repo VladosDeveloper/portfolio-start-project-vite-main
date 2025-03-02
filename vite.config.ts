43ec736393d24cf4411d0d3b15056f8d05ffb22d
VladosDeveloper <yurenya.vlad.01@mail.ru>
2025-03-02 14:47:39 +0300

    правки

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-		base: 'VladosDeveloper/portfolio-start-project-vite-main',
+		base: '/',
 		plugins: [
 				react({
 						babel: {
