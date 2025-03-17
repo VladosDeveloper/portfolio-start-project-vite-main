94aee3e5c44f1fc7831a5b86c9724eebb24f2512
VladosDeveloper <yurenya.vlad.01@mail.ru>
2025-03-17 20:50:51 +0300

    ссылки на соц. сети

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,58 +1,61 @@
-import { Icon } from '../../components/Icon/Icon'
-import { FlexWrapper } from '../../components/FlexWrapper'
-import { Container } from '../../components/Container'
-import { S } from './Footer_Styled'
-import { Link } from 'react-scroll'
+import {Icon} from '../../components/Icon/Icon'
+import {FlexWrapper} from '../../components/FlexWrapper'
+import {Container} from '../../components/Container'
+import {S} from './Footer_Styled'
+import {Link} from 'react-scroll'
 import React from "react";
 
 
 const SocialIconData = [
-	{
-		iconSrc: 'mail',
-	},
-	{
-		iconSrc: 'linkedIn',
-	},
-	{
-		iconSrc: 'gitHub',
-	},
+		{
+				iconSrc: 'habr',
+				to: 'https://career.habr.com/vladosdeveloper',
+		},
+		{
+				iconSrc: 'linkedIn',
+				to: 'https://www.linkedin.com/in/vlad-yurenya-1670b92ba/'
+		},
+		{
+				iconSrc: 'gitHub',
+				to: 'https://github.com/VladosDeveloper'
+		},
 ]
 
 export const Footer: React.FC = () => {
-	return (
-		<S.Footer>
-			<Container>
-				<FlexWrapper
-					direction='column'
-					alignItems='center'
-					justify='space-between'
-					wrap='wrap'
-				>
-					<S.SocialList>
-						{SocialIconData.map( (s, index) => {
-							return (
-								<S.SocialItem key={index}> 
-									<S.SocialLink>
-										<Icon
-											iconSrc={s.iconSrc}
-											width='40px'
-											height='40px'
-											viewbox='0 0 40 40'
-										/>
-									</S.SocialLink>
-									<span>{s.iconSrc}</span>
-								</S.SocialItem>
-							)
-						} )}
-					</S.SocialList>
-					<S.References>
-						<S.SocialLink as={Link} to='works' smooth={true} offset={-100}>Projects</S.SocialLink>
-						<S.SocialLink as={Link} to='contact' smooth={true} offset={-100}>Contact</S.SocialLink>
-					</S.References>
-					<S.Copyright>WEB DEVELOPER 2024</S.Copyright>
-				</FlexWrapper>
-			</Container>
-		</S.Footer>
-	)
+		return (
+				<S.Footer>
+						<Container>
+								<FlexWrapper
+										direction='column'
+										alignItems='center'
+										justify='space-between'
+										wrap='wrap'
+								>
+										<S.SocialList>
+												{SocialIconData.map((s, index) => {
+														return (
+																<S.SocialItem key={index} >
+																		<S.SocialLink href={s.to} >
+																				<Icon
+																						iconSrc={s.iconSrc}
+																						width='40px'
+																						height='40px'
+																						viewbox='0 0 40 40'
+																				/>
+																				<span>{s.iconSrc}</span>
+																		</S.SocialLink>
+																</S.SocialItem>
+														)
+												})}
+										</S.SocialList>
+										<S.References>
+												<S.SocialLink as={Link} to='works' smooth={true} offset={-100}>Projects</S.SocialLink>
+												<S.SocialLink as={Link} to='contact' smooth={true} offset={-100}>Contact</S.SocialLink>
+										</S.References>
+										<S.Copyright>WEB DEVELOPER 2024</S.Copyright>
+								</FlexWrapper>
+						</Container>
+				</S.Footer>
+		)
 }
 
--- src/layout/footer/Footer_Styled.ts
+++ src/layout/footer/Footer_Styled.ts
@@ -1,71 +1,81 @@
-import { styled } from "styled-components"
-import { Theme } from "../../styles/Theme"
+import {styled} from "styled-components"
+import {Theme} from "../../styles/Theme"
 
 const Footer = styled.footer`
-	display: flex;
-	min-height: 200px;
-	background-color: ${Theme.colors.primaryBG};
-	margin-bottom: 100px;
-	color: ${Theme.colors.font};
+    display: flex;
+    min-height: 200px;
+    background-color: ${Theme.colors.primaryBG};
+    margin-bottom: 100px;
+    color: ${Theme.colors.font};
 `
 const SocialList = styled.ul`
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: center;
-	gap: 72px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 72px;
 
-	@media ${Theme.media.mobile} {
-		& {
-			gap: 50px;
-		}
-	}
+    @media ${Theme.media.mobile} {
+        & {
+            gap: 50px;
+        }
+    }
 `
 const SocialItem = styled.li`
-	display: flex;
-	justify-content: center;
-	flex-direction: column;
-	align-items: center;
-	cursor: pointer;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    cursor: pointer;
 
-	span {
-		font-weight: 400;
-		font-size: 12px;
-		line-height: 0.9;
-		letter-spacing: 0.12em;
-		text-transform: uppercase;
-		text-align: center;
-	}
+    span {
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 0.9;
+        letter-spacing: 0.12em;
+        text-transform: uppercase;
+        text-align: center;
+    }
 `
 const SocialLink = styled.a`
-	color: ${Theme.colors.font};
-	padding: 10px;
-	cursor: pointer;
+    color: ${Theme.colors.font};
+    padding: 10px;
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+		transition: all .2s linear;
+		
+		&:hover {
+				color: ${Theme.colors.hoverFontColor};
+				transform: scale(110%);
+		}
 `
 const References = styled.div`
-	font-weight: 400;
-	font-size: 14px;
-	display: flex;
-	justify-content: center;
-	flex-wrap: wrap;
-	/* gap: 48px; */
+    font-weight: 400;
+    font-size: 14px;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    /* gap: 48px; */
 
-	@media ${Theme.media.mobile} {
-		gap: 0;
-		margin: 10px 0;
-	}
+    @media ${Theme.media.mobile} {
+        gap: 0;
+        margin: 10px 0;
+    }
 `
 const Copyright = styled.span`
-	font-weight: 400;
-	font-size: 14px;
-	line-height: 1.9;
-	letter-spacing: 0.04em;
-	text-align: center;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 1.9;
+    letter-spacing: 0.04em;
+    text-align: center;
 `
 export const S = {
-	Footer,
-	SocialList,
-	SocialItem,
-	SocialLink,
-	References,
-	Copyright,
+		Footer,
+		SocialList,
+		SocialItem,
+		SocialLink,
+		References,
+		Copyright,
 }
